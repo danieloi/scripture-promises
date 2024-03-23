e9f905d86797612cee68028fbf490e2986e1ac31
Mayowa Daniel <mayowadan17@gmail.com>
2024-03-23 15:06:13 +0100

    Refactor spell checking code and update OpenAI API usage

--- code/scripts/fix-spelling.py
+++ code/scripts/fix-spelling.py
@@ -1,22 +1,36 @@
-import openai
-import json
-
-# Set your OpenAI API key here
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Set your OpenAI API key here
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+
+
 
 def spell_check(text):
     try:
-        response = openai.Completion.create(
-          engine="text-davinci-003",
-          prompt=f"Correct the spelling and grammar:\n\n{text}",
-          temperature=0,
-          top_p=1.0,
-          frequency_penalty=0.0,
-          presence_penalty=0.0
+        response = client.completions.create(
+        prompt=f"Correct the spelling and grammar in this bible verse {text}. Remove all leading or trailing '\n' characters in your output. Return just a string without quote marks. Do nothing if there's no error",
+        model="gpt-3.5-turbo-instruct",
+        temperature=0.1,
+        max_tokens=3000,
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        print(f"Error during spell checking: {e}")
+        return text
+    
+def spell_check_reference(text):
+    try:
+        response = client.completions.create(
+        prompt=f"Correct the spacing/spelling if necessary: {text}. Return {text} if there's no error.",
+        model="gpt-3.5-turbo-instruct",
+        max_tokens=300,
+        temperature=0.1,
         )
         return response.choices[0].text.strip()
     except Exception as e:
@@ -29,9 +43,9 @@ def main():
         data = json.load(file)
 
     # Iterate through each item and spell check the 'quote' and 'reference'
-    for item in data[:50]:
+    for item in data[:25]:
         corrected_quote = spell_check(item['quote'])
-        corrected_reference = spell_check(item['reference'])
+        corrected_reference = spell_check_reference(item['reference'])
         
         # Update the item with corrected text
         item['quote'] = corrected_quote
@@ -40,7 +54,7 @@ def main():
 
     # Save the corrected data back to the file (or a new file)
     with open('output_corrected.json', 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()

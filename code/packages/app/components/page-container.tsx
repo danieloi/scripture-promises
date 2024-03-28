cc343043e63907553e148c9db7d5a7504a5b7894
Mayowa Daniel <mayowadan17@gmail.com>
2024-03-28 22:44:14 +0100

    Refactor page-container and sub-category-detail-screen components

--- code/packages/app/components/page-container.tsx
+++ code/packages/app/components/page-container.tsx
@@ -1,13 +1,11 @@
 import { ScrollView } from '@showtime-xyz/universal.scroll-view'
+import { View } from '@showtime-xyz/universal.view'
 
 const PageContainer = (props: any) => {
   return (
-    <ScrollView
-      tw={' bg-gray-100 p-4 md:p-10 dark:bg-gray-900'}
-      style={{ flex: 1 }}
-    >
-      {props.children}
-    </ScrollView>
+    <View tw="bg-gray-100 dark:bg-gray-900 flex-1">
+      <ScrollView tw={'  p-4 md:p-10  max-w-3xl'}>{props.children}</ScrollView>
+    </View>
   )
 }
 
--- code/packages/app/screens/sub-category-detail-screen.tsx
+++ code/packages/app/screens/sub-category-detail-screen.tsx
@@ -10,7 +10,7 @@ type SubCategoryDetailScreenProps = {
 export const SubCategoryDetailScreen: React.FC<SubCategoryDetailScreenProps> =
   withColorScheme(({ data }) => {
     return (
-      <View tw="p-4 bg-white dark:bg-black max-w-3xl rounded-2xl">
+      <View tw="p-4 bg-white dark:bg-black rounded-2xl">
         <View tw="p-4">
           <Text tw="text-base font-bold !leading-relaxed !my-0 text-gray-900 dark:text-white">
             {data.name}

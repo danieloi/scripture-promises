248b5bd7e30a86437d0f85edc914de0733c0d53f
Mayowa Daniel <mayowadan17@gmail.com>
2024-03-31 01:20:28 +0100

    run step-2 script

--- code/scripts/step-2-transform-data-with-embeddings.ts
+++ code/scripts/step-2-transform-data-with-embeddings.ts
@@ -1,22 +1,20 @@
-
 import fs from 'fs'
 // const data = JSON.parse(fs.readFileSync('apps/data.json', 'utf-8'));
 import data from '../apps/data-w-embeddings.json' assert { type: 'json' }
 
-
 function convertEmbeddingToObject(embedding) {
-  return Object.keys(embedding).map(key => embedding[key]);
+  return Object.keys(embedding).map((key) => embedding[key])
 }
 function constructDataForDb(data) {
   const { subCategories, promises } = data
 
-  const subCategoriesWithEmbedding = subCategories.map(subCategory => ({
+  const subCategoriesWithEmbedding = subCategories.map((subCategory) => ({
     ...subCategory,
     id: `subCategories-${subCategory.id}`,
     embedding: convertEmbeddingToObject(subCategory.embedding),
   }))
 
-  const promisesWithEmbedding = promises.map(promise => ({
+  const promisesWithEmbedding = promises.map((promise) => ({
     ...promise,
     id: `promises-${promise.id}`,
     embedding: convertEmbeddingToObject(promise.embedding),
@@ -28,13 +26,14 @@ function constructDataForDb(data) {
   }
 }
 
+const { subCategories, promises } = constructDataForDb(data)
 
-const { subCategories, promises } = constructDataForDb(data);
+const subCategoriesData = JSON.stringify(subCategories, null, 2)
+const promisesData = JSON.stringify(promises, null, 2)
 
-const subCategoriesData = JSON.stringify(subCategories, null, 2);
-const promisesData = JSON.stringify(promises, null, 2);
-
-
-fs.writeFileSync('apps/sub-categories-w-embeddings.json', subCategoriesData, 'utf-8')
+fs.writeFileSync(
+  'apps/sub-categories-w-embeddings.json',
+  subCategoriesData,
+  'utf-8'
+)
 fs.writeFileSync('apps/promises-w-embeddings.json', promisesData, 'utf-8')
-
